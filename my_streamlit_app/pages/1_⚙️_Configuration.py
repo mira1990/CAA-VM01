657e217ff04277a6ab486ae28415da401114d3b8
mira1990 <45895589+mira1990@users.noreply.github.com>
2025-11-17 15:45:47 +0200

    Delete my_streamlit_app directory

--- my_streamlit_app/pages/1_⚙️_Configuration.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("⚙️ Référentiel technique")
-
-st.write("Modification de la configuration du référentiel technique.")
-param = st.text_input("Paramètres", "Valeur")
-
-if st.button("Save"):
-    st.success("Saved!")
